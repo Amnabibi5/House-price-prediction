af5614393d9c6fab56d0253ffec1fee1f8eb3739
Amna Bibi <amnab9373@gmail.com>
2025-08-13 18:43:39 +0500

    Update app.py

--- app.py
+++ app.py
@@ -21,11 +21,12 @@ with open('model/scaler.pkl', 'rb') as f:
 
 # Load model
 model_path = {
-    "Linear Regression": "model/linear_model.pkl",  
+    "Linear Regression": "model/linear_regression_model.pkl",
     "KNN": "model/knn_model.pkl",
     "Random Forest": "model/random_forest_model.pkl",
     "SVM": "model/svm_model.pkl"
 }
+
 
 
 
@@ -52,6 +53,7 @@ if st.button("Predict Price"):
     st.write(f"**R² Score:** {metrics[model_choice]['R²']}")
     st.write(f"**Mean Squared Error:** {metrics[model_choice]['MSE']:,.2f}")
     st.write(f"**F1 Score:** {metrics[model_choice]['F1']}")
+
 
 
 

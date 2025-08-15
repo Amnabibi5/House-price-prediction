c87fbc36aac5f2a639957153de563b413622512c
Amna Bibi <amnab9373@gmail.com>
2025-08-15 21:20:33 +0500

    Update train_models.py

--- train_models.py
+++ train_models.py
@@ -85,5 +85,6 @@ for name, model in classification_models.items():
         ("preprocessor", preprocessor),
         ("classifier", model)
     ])
-    X_train, X_test, y_train, y_test = train_test_split(X, y_clf_encoded, test_size=0.2, random_state=42
+    X_train, X_test, y_train, y_test = train_test_split(X, y_clf_encoded, test_size=0.2, random_state=42)
+
 
